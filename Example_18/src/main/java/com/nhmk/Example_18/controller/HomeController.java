154218be7ef292c4de3f0e1ec010c4d7c214cabe
khoinhmse150853 <khoinhmse150853@fpt.edu.vn>
2022-12-18 10:31:11 +0700

    Hello World web app

---
+++ Example_18/src/main/java/com/nhmk/Example_18/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.nhmk.Example_18.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String displayHomePage() {
+        return "home.html";
+    }
+}
