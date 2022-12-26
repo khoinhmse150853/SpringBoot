71b3172899f10fe6dd728de206f786a684b8e657
khoinhmse150853 <khoinhmse150853@fpt.edu.vn>
2022-12-26 21:34:11 +0700

    Injecting Beans using Autowired

---
+++ Example_11/src/main/java/com/nhmk/Main.java
@@ -0,0 +1,23 @@
+package com.nhmk;
+
+import com.nhmk.beans.Person;
+import com.nhmk.beans.Vehicle;
+import com.nhmk.configs.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger(ProjectConfig.class.getName());
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Person person = context.getBean(Person.class);
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        logger.log(Level.INFO, "Person name from Spring Context is: {0}", person.getName());
+        logger.log(Level.INFO, "Vehicle name from Spring Context is: {0}", vehicle.getName());
+        logger.log(Level.INFO, "Vehicle that Person own is: {0}", person.getVehicle());
+    }
+}
