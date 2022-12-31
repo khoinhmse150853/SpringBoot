7644d715b8b69dee744c9f56fb11ad09d1cc54ba
khoinhmse150853 <khoinhmse150853@fpt.edu.vn>
2022-12-31 21:29:14 +0700

    Hello World Web App using Thymeleaf & DevTools Demo

---
+++ Example_19/src/main/java/com/nhmk/Example_19/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.nhmk.Example_19.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = {"", "/", "/home"})
+    public String displayHomePage(Model model) {
+        model.addAttribute("username", "John Doe");
+        return "home.html";
+    }
+}
